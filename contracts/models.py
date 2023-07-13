b45c174fe470148a957dbdad4104b0c11e7e963b
kcardon <cardon.kevin@gmail.com>
2023-07-13 15:16:08 +0200

    Implemented post_save receiver that automatically update client when contract is signed.

--- contracts/models.py
+++ contracts/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from persons.models import Client, Employee
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class ContractStatus(models.Model):
     status = models.CharField(max_length=25)
@@ -17,3 +20,12 @@ class Contract(models.Model):
     status = models.ForeignKey(ContractStatus, on_delete=models.PROTECT)
     amount = models.FloatField()
     payment_due = models.DateTimeField()
+
+
+@receiver(post_save, sender=Contract)
+def update_client_status(sender, instance, **kwargs):
+    """Met à jour la valeur "is_client" de l'objet client pour la passer en "True" dès lors qu'un contrat est signé."""
+    client = Client.objects.get(pk=instance.client.id)
+    if instance.status != "Draft" and client.is_client == False:
+        client.is_client = True
+        client.save()
--- persons/models.py
+++ persons/models.py
@@ -30,6 +30,7 @@ class Client(Person):
     email = models.CharField(max_length=250)
     company_name = models.CharField(max_length=250)
     sales_contact = models.ForeignKey(Employee, on_delete=models.PROTECT)
+    is_client = models.BooleanField(default=False)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
